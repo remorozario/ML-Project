c2fcb93a529c823e47ee6307df0d5a930a7e8764
remo rozario <renerozario48@gmail.com>
2024-03-05 01:19:32 +0530

    Setup

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-    name='mlroject',
+    name='mlproject',
     version='0.0.1',
     author='Remo',
     author_email='renerozario48@gmail.com',
